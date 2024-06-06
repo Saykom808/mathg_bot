fa37fb38ece3e81252f1500f657eb83c8ea9643c
Saykommm <saykom808@gmail.com>
2024-06-06 23:37:53 +0600

    upd

--- main.py
+++ main.py
@@ -17,20 +17,32 @@ bot = telebot.TeleBot(token)
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
-    bot.send_message(message.from_user.id, 'Привет! Я бот-калькулятор производных. Введите команду /help для получения списка команд.', reply_markup=nav.mainMenu)
+    bot.send_message(message.from_user.id, 'Привет! Я бот-калькулятор интегралов и производных. Введите команду /help для получения списка команд.', reply_markup=nav.mainMenu)
 
 # Обработчик команды /help
 @bot.message_handler(commands=['help'])
 def help(message: telebot.types.Message):
     help_text = (
         "Команды:\n"
-        "/differentiate - Вычисление производной\n"
+        "/differentiate - Вычисление производной\n\n"
+        "Правила ввода:\n"
+        "1. Выражения должны быть корректными математическими выражениями.\n"
+        "2. Используйте стандартные операторы: +, -, *, /, ** (для возведения в степень).\n"
+        "3. Для квадратного корня используйте sqrt. Например, sqrt(x).\n"
+        "4. Для экспоненты используйте exp. Например, exp(x).\n"
+        "5. Для логарифмов используйте log. Например, log(x).\n"
+        "6. Для тригонометрических функций используйте sin, cos, tan и т.д. Например, sin(x), cos(x).\n"
+        "7. Переменные должны состоять из букв.\n"
+        "Примеры корректных выражений:\n"
+        "x**2 + 2*x + 1\n"
+        "sqrt(x) + exp(x)\n"
+        "sin(x) * cos(x)\n"
     )
     bot.send_message(message.from_user.id, help_text, reply_markup=nav.mainMenu)
 
 # Валидация выражения
 def validate_expression(expression):
-    pattern = r'^[0-9a-zA-Z+\-*/^() x]*$'
+    pattern = r'^[0-9a-zA-Z+\-*/^() sqrt exp log sin cos tan]*$'
     return re.match(pattern, expression) is not None
 
 # Обработчик команды /integrate
@@ -118,9 +130,9 @@ def get_diff_variable(message: telebot.types.Message, expression):
     if variable.isalpha():
         try:
             result = differentiate_function(expression, variable)
-            bot.send_message(message.from_user.id, f"Производная {expression} по {variable} = {result}", reply_markup=nav.mainMenu)
+            bot.send_message(message.from_user.id, f"Производная {expression} по {variable}: {result}", reply_markup=nav.mainMenu)
         except Exception as e:
-            bot.send_message(message.from_user.id, f"Ошибка: {e}", reply_markup=nav.mainMenu)
+            bot.send_message(message.from_user.id, f"Ошибка: Некорректный ввод", reply_markup=nav.mainMenu)
     else:
         bot.send_message(message.from_user.id, "Некорректная переменная. Попробуйте снова.", reply_markup=nav.mainMenu)
 
